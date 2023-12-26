2d0e661fc9a77bb5f2785ca1c2d1b64d49f79a81
Boutissante <73436159+issamBoutissante@users.noreply.github.com>
2023-12-26 12:21:20 +0100

    firebase authentication app is set

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'gestion-de-reservation-fb64e',
+        appId: '1:738305311799:web:cdc5e2b3c122544ca6abb2',
+        storageBucket: 'gestion-de-reservation-fb64e.appspot.com',
+        apiKey: 'AIzaSyDncydkCpmgfdzNIETP_35QGw3SJ5c95B4',
+        authDomain: 'gestion-de-reservation-fb64e.firebaseapp.com',
+        messagingSenderId: '738305311799',
+      })
+    ),
+    provideAuth(() => getAuth()),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
