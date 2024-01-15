af6cbefdf64d78f739198edba6fdbab743f42a3c
Boutissante <73436159+issamBoutissante@users.noreply.github.com>
2024-01-15 21:18:29 +0100

    completed integrating email service

---
+++ servicedemandes/src/main/java/com/pfa/servicedemandes/service/NotificationService.java
@@ -0,0 +1,12 @@
+package com.pfa.servicedemandes;
+
+import com.pfa.servicedemandes.model.EmailMessage;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "NOTIFICATION-SERVICE")
+public interface NotificationService {
+    @PostMapping("/api/send-text")
+    public String sendTextEmail(@RequestBody EmailMessage emailMessage);
+}
